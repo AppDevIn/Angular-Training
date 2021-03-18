b419356ebd2802a4ab2aced0a42dc09f070377dd
AppDevIn <teamprojectlive@gmail.com>
2021-03-18 12:20:20 +0800

    Shopping and reciped project

---
+++ recipe-shopping-list/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './Shopping-List/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './Shopping-List/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './Recipe-Book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './Recipe-Book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './Recipe-Book/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './Header/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
