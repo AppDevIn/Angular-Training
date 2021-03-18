2ba630565e6e63956c270eaee5373e58667bf2c3
AppDevIn <teamprojectlive@gmail.com>
2021-03-18 19:35:46 +0800

    2 version of the shopping list

--- recipe-shopping-list/src/app/app.module.ts
+++ recipe-shopping-list/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './Shopping-List/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './Shopping-List/shopping-list-edit/shopping-list-edit.component';
 import { RecipeListComponent } from './Recipe-Book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './Recipe-Book/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './Recipe-Book/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './Recipe-Book/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './Header/header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { HeaderComponent } from './Header/header/header.component';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
