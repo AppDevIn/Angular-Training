9a2bfc558a808980e7d6db4d7d8b7dc97af98b78
AppDevIn <teamprojectlive@gmail.com>
2021-03-24 14:39:34 +0800

    Injectable practice done

---
+++ services/src/app/logging.service.ts
@@ -0,0 +1,8 @@
+export class LoggingService {
+  logStatusChange(status: string) {
+    console.log(
+      "🚀 ~ file: logging.service.ts ~ line 3 ~ LoggingService ~ logStatusChange ~ status. The status has changed to, ",
+      status
+    );
+  }
+}
--- services/src/app/new-account/new-account.component.ts
+++ services/src/app/new-account/new-account.component.ts
@@ -1,18 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { AccountService } from "../accounts.service";
+import { LoggingService } from "../logging.service";
 
 @Component({
-  selector: 'app-new-account',
-  templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  selector: "app-new-account",
+  templateUrl: "./new-account.component.html",
+  styleUrls: ["./new-account.component.css"],
+  providers: [LoggingService],
 })
-export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+export class NewAccountComponent implements OnInit {
+  constructor(
+    private loggingService: LoggingService,
+    private accountService: AccountService
+  ) {}
+
+  ngOnInit() {
+    this.accountService.statusUpdated.subscribe((status: string) => {
+      alert(status);
+    });
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountService.addAccount(accountName, accountStatus);
   }
 }
