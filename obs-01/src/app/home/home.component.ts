23715fcfbc291339ccac79c6eea312f8b8c40e17
AppDevIn <teamprojectlive@gmail.com>
2021-03-30 16:13:42 +0800

    Observer project done

--- obs-01/src/app/home/home.component.ts
+++ obs-01/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
+import { identifierModuleUrl } from "@angular/compiler";
 import { Component, OnDestroy, OnInit } from "@angular/core";
 
-import { interval, Subscription } from "rxjs";
+import { interval, Observable, Observer, Subscription } from "rxjs";
+import { map, filter } from "rxjs/operators";
 
 @Component({
   selector: "app-home",
@@ -17,11 +19,53 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.intervalSubscription = interval(1000).subscribe((count) => {
-      console.log(
-        "🚀 ~ file: home.component.ts ~ line 16 ~ HomeComponent ~ interval ~ count",
-        count
+    // this.intervalSubscription = interval(1000).subscribe((count) => {
+    //   console.log(
+    //     "🚀 ~ file: home.component.ts ~ line 16 ~ HomeComponent ~ interval ~ count",
+    //     count
+    //   );
+    // });
+
+    const customIntervalObservable = new Observable(
+      (observer: Observer<number>) => {
+        let count: number = 0;
+        setInterval(() => {
+          observer.next(count);
+          if (count == 2) {
+            observer.complete();
+          } else if (count > 3) {
+            observer.error(new Error("The count is graeter than 3"));
+          }
+          count++;
+        }, 1000);
+      }
+    );
+
+    this.intervalSubscription = customIntervalObservable
+      .pipe(
+        filter((data) => {
+          if (data === 1) {
+            return false;
+          }
+          return true;
+        }),
+        map((data) => {
+          return data + 1;
+        })
+      )
+      .subscribe(
+        (count) => {
+          console.log(
+            "🚀 ~ file: home.component.ts ~ line 38 ~ HomeComponent ~ customIntervalObservable.subscribe ~ count",
+            count
+          );
+        },
+        (error: Error) => {
+          alert(error.message);
+        },
+        () => {
+          console.log("Done");
+        }
       );
-    });
   }
 }
