5fe4ed59ae8e46c461ebecf509301ac3d39c84b8
AppDevIn <teamprojectlive@gmail.com>
2021-04-09 14:56:37 +0800

    Templete driven forms completed

--- form-td/src/app/app.component.ts
+++ form-td/src/app/app.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnChanges, SimpleChanges, ViewChild } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import {
+  Component,
+  HostListener,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+} from "@angular/core";
+import { NgForm, NgModel } from "@angular/forms";
+import { getAngularClassTransformerFactory } from "@angular/compiler-cli/src/transformers/r3_transform";
 
 @Component({
   selector: "app-root",
@@ -9,11 +16,40 @@ import { NgForm } from "@angular/forms";
 export class AppComponent {
   @ViewChild("f") form: NgForm;
 
+  answer = "";
+  genders = ["male", "female"];
+  user = {
+    username: "",
+    email: "",
+    secret: "",
+    answer: "",
+    gender: "",
+  };
+
   suggestUserName() {
-    const suggestedName = "Superuser";
+    // this.form.setValue({
+    //   userData: {
+    //     username: "suggestedName",
+    //     email: this.form.value["userData"]["email"],
+    //   },
+    //   secret: "pet",
+    //   questionAnswer: "",
+    //   gender: "male",
+    // });
+    this.form.form.patchValue({
+      userData: {
+        username: "SuperUser",
+      },
+    });
   }
 
   onSubmit() {
-    console.log(this.form);
+    this.user.username = this.form.value.userData.username;
+    this.user.email = this.form.value.userData.email;
+    this.user.secret = this.form.value.secret;
+    this.user.answer = this.form.value.questionAnswer;
+    this.user.gender = this.form.value.gender;
+
+    this.form.resetForm();
   }
 }
